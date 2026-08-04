7fd65d068f361744e50c8e9d6de6e8f70eac01ad
fabcctt <efabian.guzman12@gmail.com>
2026-08-04 14:01:35 -0300

    added return 0

--- Backend/bookspoo.cpp
+++ Backend/bookspoo.cpp
@@ -22,4 +22,5 @@ int main() {
     Book b2("random history 2", 400);
     std::cout << "Is b1 long? " << b1.isLong() << std::endl;
     std::cout << "Is b2 long? " << b2.isLong() << std::endl;
+    return 0;
 }
